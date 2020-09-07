fa77c20d4478e90f8c586e2335fe9e9511380da7
Lee bosung <dlqh406@gmail.com>
2020-09-08 06:32:37 +0900

    form 뼈대코드 추가 완성 : 제출버튼, color 선택 추가

--- query.js
+++ query.js
@@ -1,27 +1,34 @@
 $(document).ready(function () {});
 
-$("#prodNum").change(function () {
-  var deNum = Number($("#prodNum option:selected").val());
+$("#countSize").change(function () {
+  var deNum = Number($("#countSize option:selected").val());
+  console.log("사이즈개수 선택: " + deNum);
 
-  console.log(typeof deNum);
-  console.log(deNum);
-  $(".blowDes").html("");
+  $(".belowSize").html("");
   for (var i = 0; i < deNum; i++) {
-    var sizeArr = [
-      "Free",
-      "Small",
-      "Medium",
-      "Large",
-      "XLarge",
-      "XXLarge",
-      "XXXLarge",
-    ];
+    $(".belowSize").append(
+      ' <ul><input type="text" name="size' +
+        i +
+        '" placeholder="사이즈입력 예시)free,small,medium,22,23">' +
+        '<input type="text" name="sizeDes' +
+        i +
+        '" placeholder="실측사이즈 입력 예시)어깨 , 가슴 , 총기장 , 팔길이 ">' +
+        "</ul>"
+    );
+  }
+});
 
-    $(".blowDes").append(
-      '<ul><label for=""></label>',
-      sizeArr[i],
-      ": ",
-      '<input type="text"  placeholder="실측사이즈작성  예시)허리둘레 69cm(27.1 inch),엉덩이둘레 92cm ,전체길이 84cm"></ul>'
+$("#countColor").change(function () {
+  var deNum = Number($("#countColor option:selected").val());
+  console.log("컬러개수 선택: " + deNum);
+
+  $(".belowColor").html("");
+  for (var i = 0; i < deNum; i++) {
+    $(".belowColor").append(
+      ' <ul><input type="text" name="color' +
+        i +
+        '" placeholder="컬러입력 예시)차골">' +
+        "</ul>"
     );
   }
 });
