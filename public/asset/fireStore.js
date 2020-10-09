f44d4c7a375659ae21c32450407330595bb859dc
Lee bosung <dlqh406@gmail.com>
2020-10-10 02:14:57 +0900

    사이즈옵션 수정 // 색상옵션 수정

--- public/asset/fireStore.js
+++ public/asset/fireStore.js
@@ -1,13 +1,51 @@
+$(document).ready(function () {});
+
+$("#countSize").change(function () {
+  sdeNum = Number($("#countSize option:selected").val());
+  console.log("사이즈개수 선택: " + sdeNum);
+
+  $(".belowSize").html("");
+  for (var i = 0; i < sdeNum; i++) {
+    $(".belowSize").append(
+      ' <ul><input type="text" id="size' +
+        i +
+        '" placeholder="사이즈입력 예시)free,small,medium,22,23">' +
+        "</ul>"
+    );
+  }
+});
+
+$("#countColor").change(function () {
+  deNum = Number($("#countColor option:selected").val());
+  console.log("컬러개수 선택: " + deNum);
+
+  $(".belowColor").html("");
+  for (var i = 0; i < deNum; i++) {
+    $(".belowColor").append(
+      ' <ul><input type="text" id="color' +
+        i +
+        '"placeholder="컬러입력 예시)차골">' +
+        "</ul>"
+    );
+  }
+});
+
+var sdeNum;
+var deNum;
 imgList = [];
+var _sizeList = [];
+var _colorList = [];
 
 function postToFirStore() {
-  var sizeList = [];
+  for (var i = 0; i < deNum; i++) {
+    _colorList.push($("#color" + i).val());
+  }
 
-  // for (var i=0; i<7; i++){
-  //   if($("#size"+i).val()=undefined){
-  //     console.log($("#size"+i).val())
-  //   }
-  // }
+  for (var i = 0; i < sdeNum; i++) {
+    _sizeList.push($("#size" + i).val());
+  }
+
+  console.log(_sizeList);
 
   if (!imgList[1]) {
     alert("첨부한 이미지의 저장버튼을 누르세요");
@@ -30,44 +68,10 @@ function postToFirStore() {
         ODD_can: $("#onedaydeli_can").is(":checked"),
         uploadDate: _uploadDate,
         productDecription: _resultQuill,
-
-        sizeList: [
-          typeof $("#size0").val() == "undefined" ? null : $("#size0").val(),
-          typeof $("#size1").val() == "undefined" ? null : $("#size1").val(),
-          typeof $("#size2").val() == "undefined" ? null : $("#size2").val(),
-          typeof $("#size3").val() == "undefined" ? null : $("#size3").val(),
-          typeof $("#size4").val() == "undefined" ? null : $("#size4").val(),
-          typeof $("#size5").val() == "undefined" ? null : $("#size5").val(),
-          typeof $("#size6").val() == "undefined" ? null : $("#size6").val(),
-        ],
-        sizeDesList: [
-          typeof $("#sizeDes0").val() == "undefined"
-            ? null
-            : $("#sizeDes0").val(),
-          typeof $("#sizeDes1").val() == "undefined"
-            ? null
-            : $("#sizeDes1").val(),
-          typeof $("#sizeDes2").val() == "undefined"
-            ? null
-            : $("#sizeDes2").val(),
-          typeof $("#sizeDes3").val() == "undefined"
-            ? null
-            : $("#sizeDes3").val(),
-          typeof $("#sizeDes4").val() == "undefined"
-            ? null
-            : $("#sizeDes4").val(),
-          typeof $("#sizeDes5").val() == "undefined"
-            ? null
-            : $("#sizeDes5").val(),
-          typeof $("#sizeDes6").val() == "undefined"
-            ? null
-            : $("#sizeDes6").val(),
-        ],
+        sizeList: _sizeList,
+        colorList: _colorList,
         style: $("#StyleList option:selected").val(),
         category: $("#category option:selected").val(),
-
-        // StyleList:
-        // TypeList:
       });
   }
   console.log("uploade completed");
@@ -80,6 +84,7 @@ function create_detailUrl() {
     // send to img obj
     onloadImage(this);
   });
+
   function onloadImage(input) {
     if (input.files && input.files[0]) {
       var items = input.files[0];
@@ -91,13 +96,15 @@ function create_detailUrl() {
     }
   }
   $("#belowDetail").html("<span>상세 이미지 저장완료</span>");
+
   function saveToStorage(items) {
     var storageRef = firebase.storage().ref();
     var _name = items.name.replace(
       /[~`!#$%\^&*+=\-\[\]\\';,/{}()|\\":<>\?]/g,
       ""
     );
-    var uploadTask = storageRef.child("data/" + "/" + _name).put(items);
+    //ml로 바꿈 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    var uploadTask = storageRef.child("ml/" + "/" + _name).put(items);
     // .child("data/" + user.uid + "/" + _name)
 
     uploadTask.on(
